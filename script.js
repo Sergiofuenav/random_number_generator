2426a1a0f38cc7538e386a9dc3fd8d0f650e631a
Sergio <sergiofuenav@gmail.com>
2023-08-24 12:13:50 +0200

    Compressed images

--- script.js
+++ script.js
@@ -101,6 +101,18 @@ const casillero = [
   "Papa",
 ];
 
+const preloadedImages = [];
+for (let i = 0; i < 100; i++) {
+  if (i >= 10 && i < 30) {
+    preloadedImages.push(null);
+    continue;
+  }
+  const img = new Image();
+  const imageName = i.toString().padStart(2, "0"); // Format the image name
+  img.src = `images/${imageName}.png`; // Adjust the path based on your images' actual location
+  preloadedImages.push(img);
+}
+
 function generateForm() {
   const randomNumber = Math.floor(Math.random() * 8) + 3; // Generate number between 3 and 10
   return randomNumber === 10 ? 0 : randomNumber;
@@ -120,6 +132,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let interval;
 
+  let numbers = [];
   goButton.addEventListener("click", function () {
     const timeout = parseInt(timeoutInput.value);
     const showTime = parseInt(showTimeInput.value);
@@ -132,11 +145,23 @@ document.addEventListener("DOMContentLoaded", function () {
     interval = setInterval(function () {
       if (counter >= amount) {
         clearInterval(interval);
+        console.log(numbers);
+        numbers = [];
         return;
       }
 
       let randomNumber;
       let binaryDigits = 0;
+
+      const fontSize = parseInt(fontSizeInput.value);
+      const isBold = boldCheckbox.checked;
+      const muestraCasillaALaVez = muestraCasillaElement.checked;
+
+      const numberElement = document.createElement("div");
+      numberElement.style.fontSize = `${fontSize}px`; // Apply font size
+      numberElement.style.fontWeight = isBold ? "bold" : "normal"; // Apply font weight
+      numberElement.classList.add("number");
+      numbersContainer.appendChild(numberElement);
 
       switch (format) {
         case "binary6":
@@ -155,24 +180,28 @@ document.addEventListener("DOMContentLoaded", function () {
           const form = generateForm();
           const color = Math.floor(Math.random() * 9);
           randomNumber = `${form}${color}`;
-          content = `<img src="images/${randomNumber}.png" alt="${randomNumber}" />`;
+
+          if (muestraCasillaALaVez) {
+            const wordElement = document.createElement("div");
+            let wordIdx = randomNumber % casillero.length;
+            if (format.includes("bin")) {
+              wordIdx = parseInt(randomNumber, 2);
+            }
+            wordElement.textContent = casillero[wordIdx];
+            wordElement.classList.add("word");
+            numberElement.appendChild(wordElement);
+          }
+          const imgElement = document.createElement("img");
+          imgElement.src = preloadedImages[parseInt(randomNumber)].src;
+          imgElement.alt = randomNumber;
+          numberElement.appendChild(imgElement);
           break;
         default:
           randomNumber = Math.floor(Math.random() * 100)
             .toString()
             .padStart(2, "0");
       }
-
-      const fontSize = parseInt(fontSizeInput.value);
-      const isBold = boldCheckbox.checked;
-      const muestraCasillaALaVez = muestraCasillaElement.checked;
-
-      const numberElement = document.createElement("div");
-      numberElement.style.fontSize = `${fontSize}px`; // Apply font size
-      numberElement.style.fontWeight = isBold ? "bold" : "normal"; // Apply font weight
-      numberElement.classList.add("number");
-      numbersContainer.appendChild(numberElement);
-      console.log(randomNumber)
+      numbers.push(randomNumber);
 
       // Display binary numbers in rows
       if (format === "binary6" || format === "binary8") {
@@ -185,14 +214,12 @@ document.addEventListener("DOMContentLoaded", function () {
           rowElement.textContent = randomNumber.slice(start, end);
           numberElement.appendChild(rowElement);
         }
-      } else if (format === "figures") {
-        numberElement.innerHTML = content;
-      } else {
+      } else if (format === "decimal") {
         numberElement.textContent = randomNumber;
       }
 
-      const wordElement = document.createElement("div");
-      if (muestraCasillaALaVez) {
+      if (muestraCasillaALaVez && format !== "figures") {
+        const wordElement = document.createElement("div");
         let wordIdx = randomNumber % casillero.length;
         if (format.includes("bin")) {
           wordIdx = parseInt(randomNumber, 2);
